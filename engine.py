ada5513e87ba4a90a7502b55dc637a53324b2ef6
shreypithava <shreygalaxy3@gmail.com>
2019-10-02 00:24:39 -0500

    fixed and getting random names

--- engine.py
+++ engine.py
@@ -6,16 +6,16 @@ class Engine(object):
     def __init__(self):
         self.blue_team = list()
         self.red_team = list()
+        self.__get_players()
 
     def __get_players(self):  # complete this function
-        pass
+        for _ in range(22):
+            self.blue_team.append(Player(personal=Personal(name=str(random.randint(10000, 100000)))))
+            self.red_team.append(Player(personal=Personal(name=str(random.randint(10000, 100000)))))
 
 
 if __name__ == '__main__':
     engine = Engine()
-    for _ in range(22):
-        engine.blue_team.append(Player(personal=Personal(name=str(random.randint(10000, 100000)))))
-        engine.red_team.append(Player(personal=Personal(name=str(random.randint(10000, 100000)))))
 
     for player in engine.red_team + engine.blue_team:
         print(player.get_personal().get_name())
